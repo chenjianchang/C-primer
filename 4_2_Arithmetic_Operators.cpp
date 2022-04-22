97b3fa4cda65d47aa05e6a269124371f655c92ce
tom <shaolang@163.com>
2022-04-22 12:46:02 +0800

    add a file

---
+++ 4_2_Arithmetic_Operators.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+
+    int i = 1024;
+    int k = -i;
+    bool b = true;
+    bool b2 = -b;
+
+    short short_value = 32767;
+    short_value += 1;
+    cout << "short_value:" << short_value << endl;
+
+    return 0;
+
+    
+}
